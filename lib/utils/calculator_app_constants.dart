45eb4bbb34a0f7d9bb6f9e196500eec413963ced
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-03 17:30:24 +0300

    Create calculator_app_constants.dart

---
+++ lib/utils/calculator_app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CalculatorAppConstants {
+  static const double defaultPadding = 10.0;
+  static SizedBox defaultHeightBox() => const SizedBox(height: defaultPadding);
+}
