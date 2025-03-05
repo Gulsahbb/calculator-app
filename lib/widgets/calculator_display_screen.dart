15ff6fb8e54ca96678b462045ba2264c25ad71de
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-05 20:10:40 +0300

    New design for display screen

--- lib/widgets/calculator_display_screen.dart
+++ lib/widgets/calculator_display_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/utils/calculator_app_styles.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorDisplayScreen extends StatelessWidget {
@@ -7,9 +8,12 @@ class CalculatorDisplayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: 3,
       child: Container(
-        alignment: Alignment.bottomRight,
-        padding: const EdgeInsets.all(20),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.all(16),
+        decoration: CalculatorAppStyles.displayDecoration,
         child: Text(
           displayText,
           style: Theme.of(context).textTheme.displayLarge,
