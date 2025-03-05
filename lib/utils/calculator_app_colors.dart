682c86d6b40544729e4ba7c9dfc28e9b07b9bf31
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-05 20:10:08 +0300

    Test App Colors

---
+++ lib/utils/calculator_app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CalculatorAppColors {
+  static const Color appBackground = Color.fromARGB(255, 72, 43, 85);
+  static const Color backgroundGreen = Color.fromARGB(255, 144, 222, 111);
+  static const Color buttonBackground = Color.fromARGB(255, 96, 77, 105);
+  static const Color buttonShadowLight = Color(0xFFFFFFFF);
+  static const Color buttonShadowDark = Color(0xFFC7D1BC);
+  static const Color textColor = Color.fromARGB(255, 204, 204, 204);
+  static const Color operatorColor = Color(0xFF4CAF50);
+}
