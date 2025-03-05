b9da53e80a9fbc164abdf463a8f852de275e3e8a
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-05 20:11:14 +0300

    New design for button panel

--- lib/widgets/calculator_button_panel.dart
+++ lib/widgets/calculator_button_panel.dart
@@ -3,17 +3,59 @@ import 'package:flutter/material.dart';
 
 class CalculatorButtonPanel extends StatelessWidget {
   final Function(String) onButtonPressed;
-  const CalculatorButtonPanel({super.key, required this.onButtonPressed});
+  CalculatorButtonPanel({super.key, required this.onButtonPressed});
 
-  final List<List<String>> buttons = const [
+  /* final List<List<String>> buttons = const [
+    ["C", "()", "%", "^"],
     ["7", "8", "9", "/"],
-    ["4", "5", "6", "×"],
+    ["4", "5", "6", "x"],
     ["1", "2", "3", "-"],
-    ["C", "0", "=", "+"],
+    [",", "0", "=", "+"],
+  ];*/
+  final buttons = [
+    "C",
+    "()",
+    "%",
+    "^",
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "x",
+    "1",
+    "2",
+    "3",
+    "-",
+    ",",
+    "0",
+    "=",
+    "+",
   ];
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Expanded(
+      flex: 5,
+      child: GridView.builder(
+        padding: EdgeInsets.all(16),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          childAspectRatio: 1,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return CalculatorButton(
+            text: buttons[index],
+            onTap: () => onButtonPressed(buttons[index]),
+          );
+        },
+      ),
+    );
+    /* return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children:
           buttons.map((row) {
@@ -34,6 +76,6 @@ class CalculatorButtonPanel extends StatelessWidget {
                   }).toList(),
             );
           }).toList(),
-    );
+    );*/
   }
 }
