4d474718f9392bda3a1859787544b94c4810ec0b
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-07 01:17:01 +0300

    Change gestureDetector with Inkwell

--- lib/widgets/calculator_button.dart
+++ lib/widgets/calculator_button.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/utils/calculator_app_colors.dart';
 import 'package:calculator/utils/calculator_app_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -14,44 +15,49 @@ class CalculatorButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(20),
+        child: Ink(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color:
+                isOperator
+                    ? CalculatorAppColors.operatorButtonBackground
+                    : CalculatorAppColors.buttonBackground,
+
+            /*      boxShadow: [
+              BoxShadow(
+                color: CalculatorAppColors.buttonShadowLight,
+                offset: const Offset(1, 1),
+                blurRadius: 1,
+              ),
+              BoxShadow(
+                color: CalculatorAppColors.buttonShadowDark,
+                offset: const Offset(1, 1),
+                blurRadius: 1,
+              ),
+            ],*/
+          ),
+          child: Center(
+            child: Text(text, style: CalculatorAppStyles.numberTextStyle),
+          ),
+        ),
+      ),
+    );
+    /*   return GestureDetector(
       onTap: onTap,
       child: Container(
-        decoration: CalculatorAppStyles.buttonDecoration,
+        decoration:
+            isOperator
+                ? CalculatorAppStyles.operatorButtonBackground
+                : CalculatorAppStyles.numberButtonBackground,
         child: Center(
-          child: Text(
-            text,
-            style:
-                isOperator
-                    ? CalculatorAppStyles.operatorTextStyle
-                    : CalculatorAppStyles.numberTextStyle,
-          ),
+          child: Text(text, style: CalculatorAppStyles.numberTextStyle),
         ),
       ),
-    );
-    /*  return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.secondaryFixedDim,
-            offset: Offset(1, 2),
-            blurRadius: 5,
-          ),
-        ],
-      ),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Theme.of(context).colorScheme.secondary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        onPressed: onTap,
-        child: Text(text, style: Theme.of(context).textTheme.headlineMedium),
-      ),
-    );
-*/
+    );*/
   }
 }
