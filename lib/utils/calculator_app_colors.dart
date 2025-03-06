07c67725dccdc4a76698f79eba3479ba172c4f09
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-07 01:15:35 +0300

    Change color

--- lib/utils/calculator_app_colors.dart
+++ lib/utils/calculator_app_colors.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 class CalculatorAppColors {
   static const Color appBackground = Color.fromARGB(255, 72, 43, 85);
-  static const Color backgroundGreen = Color.fromARGB(255, 144, 222, 111);
+  static const Color backgroundGreen = Color.fromARGB(255, 136, 227, 97);
   static const Color buttonBackground = Color.fromARGB(255, 96, 77, 105);
-  static const Color buttonShadowLight = Color(0xFFFFFFFF);
-  static const Color buttonShadowDark = Color(0xFFC7D1BC);
-  static const Color textColor = Color.fromARGB(255, 204, 204, 204);
-  static const Color operatorColor = Color(0xFF4CAF50);
+  static const Color buttonShadowLight = Color.fromARGB(255, 255, 255, 255);
+  static const Color buttonShadowDark = Color.fromARGB(255, 199, 209, 188);
+  static const Color textColor = Color.fromARGB(255, 248, 248, 248);
+  static const Color operatorButtonBackground = Color.fromARGB(
+    255,
+    251,
+    118,
+    154,
+  );
 }
