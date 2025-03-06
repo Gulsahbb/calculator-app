cad0b0e055155753b9facfe39f82d8bf2f4dfd83
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-07 01:16:33 +0300

    Update calculator_app_styles.dart

--- lib/utils/calculator_app_styles.dart
+++ lib/utils/calculator_app_styles.dart
@@ -2,22 +2,30 @@ import 'package:calculator/utils/calculator_app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorAppStyles {
-  static final BoxDecoration buttonDecoration = BoxDecoration(
-    color: CalculatorAppColors.buttonBackground,
-    borderRadius: BorderRadius.circular(20),
-    boxShadow: [
-      BoxShadow(
-        color: CalculatorAppColors.buttonShadowLight,
-        offset: const Offset(1, 1),
-        blurRadius: 1,
-      ),
-      BoxShadow(
-        color: CalculatorAppColors.buttonShadowDark,
-        offset: const Offset(1, 1),
-        blurRadius: 1,
-      ),
-    ],
-  );
+  static BoxDecoration buttonDecoration({required Color color}) {
+    return BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: CalculatorAppColors.buttonShadowLight,
+          offset: const Offset(1, 2),
+          blurRadius: 1,
+        ),
+        BoxShadow(
+          color: CalculatorAppColors.buttonShadowDark,
+          offset: const Offset(1, 2),
+          blurRadius: 1,
+        ),
+      ],
+    );
+  }
+
+  static BoxDecoration get numberButtonBackground =>
+      buttonDecoration(color: CalculatorAppColors.buttonBackground);
+  static BoxDecoration get operatorButtonBackground =>
+      buttonDecoration(color: CalculatorAppColors.operatorButtonBackground);
+
   static final BoxDecoration displayDecoration = BoxDecoration(
     color: CalculatorAppColors.backgroundGreen,
     borderRadius: BorderRadius.circular(20),
@@ -37,12 +45,6 @@ class CalculatorAppStyles {
   static final TextStyle numberTextStyle = TextStyle(
     color: CalculatorAppColors.textColor,
     fontSize: 32,
-    fontWeight: FontWeight.w500,
-  );
-
-  static final TextStyle operatorTextStyle = TextStyle(
-    color: CalculatorAppColors.operatorColor,
-    fontSize: 24,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.bold,
   );
 }
