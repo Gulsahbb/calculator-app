89a93794da830dceff5ee7ca4486c9b02846a1bd
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-09 17:12:33 +0300

    Add _CreateButton widget

--- lib/widgets/calculator_button_panel.dart
+++ lib/widgets/calculator_button_panel.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/utils/calculator_app_constants.dart';
 import 'package:calculator/widgets/calculator_button.dart';
 import 'package:flutter/material.dart';
 
@@ -5,13 +6,6 @@ class CalculatorButtonPanel extends StatelessWidget {
   final Function(String) onButtonPressed;
   CalculatorButtonPanel({super.key, required this.onButtonPressed});
 
-  /* final List<List<String>> buttons = const [
-    ["C", "()", "%", "^"],
-    ["7", "8", "9", "/"],
-    ["4", "5", "6", "x"],
-    ["1", "2", "3", "-"],
-    [",", "0", "=", "+"],
-  ];*/
   final List<String> buttons = [
     'C',
     '+/-',
@@ -51,24 +45,45 @@ class CalculatorButtonPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 5,
-      child: GridView.builder(
-        padding: EdgeInsets.all(16),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4, // Sütun sayısı
-          //childAspectRatio: 1,
-          crossAxisSpacing: 16, // Sütunlar arası boşluk
-          mainAxisSpacing: 16, // satırlar arası boşluk
-        ),
-        itemCount: 20, // item sayısı
-        itemBuilder: (context, index) {
-          bool isOperator = operators.contains(buttons[index]);
-          return CalculatorButton(
-            text: buttons[index],
-            isOperator: isOperator,
-            onTap: () => onButtonPressed(buttons[index]),
-          );
-        },
+      child: _CreateButton(
+        operators: operators,
+        buttons: buttons,
+        onButtonPressed: onButtonPressed,
       ),
+    );
+  }
+}
+
+class _CreateButton extends StatelessWidget {
+  const _CreateButton({
+    required this.operators,
+    required this.buttons,
+    required this.onButtonPressed,
+  });
+
+  final List<String> operators;
+  final List<String> buttons;
+  final Function(String p1) onButtonPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: CalculatorAppConstants.defaultPadding,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4, // Sütun sayısı
+        //childAspectRatio: 1,
+        crossAxisSpacing: 16, // Sütunlar arası boşluk
+        mainAxisSpacing: 16, // satırlar arası boşluk
+      ),
+      itemCount: 20, // item sayısı
+      itemBuilder: (context, index) {
+        bool isOperator = operators.contains(buttons[index]);
+        return CalculatorButton(
+          text: buttons[index],
+          isOperator: isOperator,
+          onTap: () => onButtonPressed(buttons[index]),
+        );
+      },
     );
   }
 }
