4989ded7e2a61f319f0e654d9cdc823a02166a0a
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-03-09 17:11:28 +0300

    Add constants val boxshadow,padding,margin

--- lib/utils/calculator_app_constants.dart
+++ lib/utils/calculator_app_constants.dart
@@ -1,6 +1,21 @@
+import 'package:calculator/utils/calculator_app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorAppConstants {
-  static const double defaultPadding = 10.0;
-  static SizedBox defaultHeightBox() => const SizedBox(height: defaultPadding);
+  static const EdgeInsets defaultPadding = EdgeInsets.all(20);
+  static const EdgeInsets defaultMargin = EdgeInsets.all(16);
+  static BorderRadius defaultBorderRadius = BorderRadius.circular(20);
+  static List<BoxShadow> defaultBoxShadow = [
+    BoxShadow(
+      color: CalculatorAppColors.buttonShadowLight,
+      offset: const Offset(1, 2),
+      blurRadius: 1,
+    ),
+    BoxShadow(
+      color: CalculatorAppColors.buttonShadowDark,
+      offset: const Offset(1, 2),
+      blurRadius: 1,
+    ),
+  ];
+  static SizedBox defaultHeightBox() => const SizedBox(height: 10);
 }
